d55a1e2ad335c2c62e057e0b0f635ca06736ffc6
Leandro Alfonso <lalfonso@gmail.com>
2013-07-11 17:31:50 -0300

    UI image inclussion

--- overlay_images/app/models/overlay.rb
+++ overlay_images/app/models/overlay.rb
@@ -2,8 +2,14 @@ class Overlay < ActiveRecord::Base
   attr_accessible :date_from, :date_to, :image, :position, :remote_image_url
   mount_uploader :image, ImageUploader
 
-  POSITIONS = %w(lt lb rt rb)
+  POSITIONS = {'Left Top' => 'lt', 'Left Bottom' => 'lb', 'Right Top' => 'rt', 'Right Bottom' => 'rb'}
 
-  scope :current, where("? BETWEEN date_from AND date_to", Time.now).first
+  def self.current
+    where("? BETWEEN date_from AND date_to", Time.now).first
+  end
+
+  before_save do |r|
+    r.position ||= 'rt'
+  end
 
 end
