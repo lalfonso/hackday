5f6183dafc7a6e61a39135f7e4ad97731b5d736b
Geronimo Diaz <geronimo@gdiaz.(none)>
2013-07-11 16:07:43 -0300

    Add current scope to find overlay

--- overlay_images/app/models/overlay.rb
+++ overlay_images/app/models/overlay.rb
@@ -2,4 +2,7 @@ class Overlay < ActiveRecord::Base
   attr_accessible :date_from, :date_to, :image, :position
 
   POSITIONS = %w(UL UR BL BR)
+
+  scope :current, where("? BETWEEN date_from AND date_to", Time.now).first
+
 end
