17d6c3070d16840fd697938d63391f5e5aeb19ad
Sergio D. Márquez <smarquez@devspark.com>
2013-07-11 16:11:50 -0300

    more changes

--- overlay_images/app/models/overlay.rb
+++ overlay_images/app/models/overlay.rb
@@ -1,5 +1,6 @@
 class Overlay < ActiveRecord::Base
-  attr_accessible :date_from, :date_to, :image, :position
+  attr_accessible :date_from, :date_to, :image, :position, :remote_image_url
+  mount_uploader :image, ImageUploader
 
-  POSITIONS = %w(UL UR BL BR)
+  POSITIONS = %w(lt lb rt rb)
 end
