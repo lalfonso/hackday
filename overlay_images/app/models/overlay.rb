47a0b612359a71103049044df42f385fb276e377
Leandro Alfonso <lalfonso@gmail.com>
2013-07-12 01:39:51 -0300

    merging

--- overlay_images/app/models/overlay.rb
+++ overlay_images/app/models/overlay.rb
@@ -1,5 +1,5 @@
 class Overlay < ActiveRecord::Base
-  attr_accessible :date_from, :date_to, :image, :position
+  attr_accessible :date_from, :date_to, :image, :position, :description
   scope :current, where("? BETWEEN date_from AND date_to", Time.now).first
   
   mount_uploader :image, ImageUploader
